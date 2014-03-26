d03f3ea5663a6869ea23e9bbe625562b31115a76
fabri <fabri@inventati.org>
2014-03-26 16:25:43 +0100

    logos

--- partners/admin.py
+++ partners/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from partners.models import *
 
-# Register your models here.
 admin.site.register(LPIUser)
-# Register your models here.
+admin.site.register(LPIRule)
+admin.site.register(LPIIndexes)
+
+
